1133f6380deaea56f7c69cdb786615b912a55ca2
William Prasetyo Utomo <williamellio05@gmail.com>
2025-11-15 09:45:34 +0700

    main

---
+++ src/main/java/com/mycompany/tr_ast_pbo/TR_AST_PBO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tr_ast_pbo;
+
+/**
+ *
+ * @author Windows
+ */
+public class TR_AST_PBO {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
