106b6a918b1a76463364daad26aebed946d8c493
Made Khanaya Dew Astawa <khanaydew123@gmail.com>
2025-12-03 23:14:14 +0700

    login page

---
+++ view/src/main/java/com/mycompany/tr_ast_pbo/DarkMode.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.tr_ast_pbo;
+
+/**
+ *
+ * @author Made
+ */
+public class DarkMode {
+    public static boolean isDarkMode = false;
+}
